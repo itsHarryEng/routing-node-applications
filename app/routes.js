2f56b0ed265361e5f2b8a41404b90d312975322a
Harry Eng <harry@harryeng.com>
2016-11-24 20:16:44 -0500

    Routing Node Applications (Scotch.io)

---
+++ app/routes.js
@@ -0,0 +1,25 @@
+var express             = require('express'),
+    router              = express.Router(),
+    path                = require('path'),
+    siteController      = require('./controllers/site.controller'),
+    dashboardController = require('./controllers/dashboard.controller')
+    nameCheckMiddleware = require('./middleware/nameCheck');
+
+// export the router
+module.exports = router;
+
+// site routes ======================
+router.get('/',         siteController.showHome);
+router.get('/about',    siteController.showAbout);
+router.get('/contact',  siteController.showContact);
+router.post('/contact', siteController.processContact);
+router.get('/@:username/:post_slug', nameCheckMiddleware, siteController.showProfile);
+
+// dashboard routes =================
+router.get('/dashboard', dashboardController.showDashboard);
+
+// api routes =======================
+
+
+// 404 catchall
+router.use(siteController.show404);
---
+++ server.js
@@ -0,0 +1,16 @@
+var app = require('express')(),
+    port = process.env.PORT || 8080,
+    morgan = require('morgan'),
+    bodyParser = require('body-parser');
+
+// configure
+app.use(morgan('dev'));
+app.use(bodyParser.urlencoded({ extended: true }));
+
+// set routes
+app.use(require('./app/routes'));
+
+// start the server
+app.listen(port, function () {
+   console.log('App listening on http://localhost: ' + port);
+});
